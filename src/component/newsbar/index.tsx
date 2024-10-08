2b70c3f55fb60d2a7591a288c0e6762d77d63929
Samuel Ronick <ronicksamuel@gmail.com>
2024-10-08 01:35:28 +0100

    update portfolio

--- src/component/newsbar/index.tsx
+++ src/component/newsbar/index.tsx
@@ -2,7 +2,7 @@ import styles from "./newsbar.module.css";
 export default function NewsBar() {
   return (
     <div className={styles["newsbar"]}>
-      <span>welcome to my portfolio 📞 </span>
+      <span className={styles["mb-span"]}>welcome to my portfolio 📞 </span>
       <span className={styles["newsbarLinks"]}>
         <a href="https://wa.link/wlimfr"> WhatSapp</a>{" "}
         <a href="mailto:ronicksamuel@gmail.com">Email</a>{" "}
